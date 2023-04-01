5525fffae26842fa1c4dfcb1963850aa23df678e
ShingoYadomoto <shingo.yadomoto@gmail.com>
2023-04-01 19:28:01 +0900

    stage 3

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
 	"fmt"
 	"io"
 	"os"
@@ -30,6 +32,7 @@ func main() {
 		}
 
 		fmt.Println("Initialized git directory")
+
 	case "cat-file":
 		option := os.Args[2]
 		if option == "-p" {
@@ -58,6 +61,105 @@ func main() {
 
 			splitContent := strings.Split(string(content), "\x00")
 			fmt.Print(splitContent[1])
+		} else {
+			fmt.Fprintf(os.Stderr, "Unknown option %s\n", option)
+			os.Exit(1)
+		}
+
+	case "hash-object":
+		option := os.Args[2]
+		if option == "-w" {
+
+			contentBytes, err := func() ([]byte, error) {
+				var (
+					file = os.Args[3]
+				)
+				f, err := os.Open(file)
+				if err != nil {
+					return nil, err
+				}
+				defer f.Close()
+
+				content, err := io.ReadAll(f)
+				if err != nil {
+					return nil, err
+				}
+
+				var (
+					header      = append([]byte("blob\x00"+fmt.Sprint(len(content))), []byte{0}...)
+					fullContent = append(header, content...)
+				)
+
+				return fullContent, nil
+			}()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, err.Error())
+				return
+			}
+
+			compressedContentBytes, err := func() ([]byte, error) {
+				var b bytes.Buffer
+
+				w := zlib.NewWriter(&b)
+				defer w.Close()
+
+				_, err = w.Write(contentBytes)
+				if err != nil {
+					return nil, err
+				}
+
+				return b.Bytes(), nil
+			}()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, err.Error())
+				return
+			}
+
+			blobSHA, err := func() (string, error) {
+				h := sha1.New()
+
+				_, err := h.Write(contentBytes)
+				if err != nil {
+					return "", err
+				}
+
+				blobSHABytes := h.Sum(nil)
+
+				return fmt.Sprintf("%x\n", blobSHABytes), nil
+			}()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, err.Error())
+				return
+			}
+
+			err = func() error {
+				var (
+					dir  = filepath.Join(".git", "objects", blobSHA[:2])
+					file = filepath.Join(dir, blobSHA[2:])
+				)
+				if err := os.MkdirAll(dir, 0755); err != nil {
+					return err
+				}
+
+				f, err := os.Create(file)
+				if err != nil {
+					return err
+				}
+				defer f.Close()
+
+				_, err = f.Write(compressedContentBytes)
+				if err != nil {
+					return err
+				}
+
+				return nil
+			}()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, err.Error())
+				return
+			}
+
+			fmt.Print(blobSHA)
 		} else {
 			fmt.Fprintf(os.Stderr, "Unknown option %s\n", option)
 			os.Exit(1)
